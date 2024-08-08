4e93bcaaa794df25669228b888f937f5cba581a0
FlorianSeliger7 <florian.seliger@gmail.com>
2024-08-08 10:31:14 +0300

    implemented Azure function Api

--- frontend/main.js
+++ frontend/main.js
@@ -2,19 +2,19 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = '';
+const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
-    let count = 0;
-    fetch (functionApi).then(response => {
-        return response.json()
-    }).then(response =>{
-        console.log("Website called function API.");
-        count = response.count;
-        document.getElementById("counter").innerText = count;
-
-    }).catch(function(error){
-        console.log(error);
-    });
-    return count;
-}
+    return fetch(functionApi)
+        .then(response => response.json())
+        .then(response => {
+            console.log("Website called function API.");
+            const count = response.count;
+            document.getElementById("Counter").innerText = count;
+            return count;
+        })
+        .catch(error => {
+            console.error(error);
+            return 1000;  // Default value if fetch fails
+        });
+};
