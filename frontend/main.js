5e5c2f316ddf2f7155660e72d37d3e6fdbf1da65
FlorianSeliger7 <florian.seliger@gmail.com>
2024-08-08 12:51:41 +0300

    redirected  api from local to cloud

--- frontend/main.js
+++ frontend/main.js
@@ -2,10 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
+
+const functionApiUrl = "https://cvdbcounter.azurewebsites.net/api/GetResumeCounter?code=2uG25IPMFzee0E63iHLmSELVmYQy6Y6hT6rIL-95dtuiAzFuHeW-yw%3D%3D";
 const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
-    return fetch(functionApi)
+    return fetch(functionApiUrl)
         .then(response => response.json())
         .then(response => {
             console.log("Website called function API.");
