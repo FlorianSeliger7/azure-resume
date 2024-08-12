ec0036c4a03c52e817847d5a1e63dea95cf8acb7
FlorianSeliger7 <florian.seliger@gmail.com>
2024-08-12 15:47:53 +0300

    Updated frontend: Updated CV to be cloud specific

--- backend/tests/obj/Debug/net6.0/tests.AssemblyInfo.cs
+++ backend/tests/obj/Debug/net6.0/tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+834b5a74c81b081080e1790ad64b0b24c39f0f76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc6c4e5c77c4f429efcfcc35e1c320787f5b13ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
